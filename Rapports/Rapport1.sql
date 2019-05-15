1f86acf8251ea8fe14dd96e2633a15eea13ed866
GratinDauphinoi <43410003+GratinDauphinoi@users.noreply.github.com>
2019-05-15 17:24:53 +0200

    Update Rapport1.sql

--- Rapports/Rapport1.sql
+++ Rapports/Rapport1.sql
@@ -3,7 +3,6 @@ FROM t_employee e
 JOIN t_reservation r
 ON e.employee_id = r.employee_id
 GROUP BY (e.first_name  ||' '||  e.last_name)
-ORDER BY Réservation DESC
-FETCH FIRST 1 ROWS ONLY;
+ORDER BY Réservation DESC;
 
 (ca affiche que le derniere samsoul)
