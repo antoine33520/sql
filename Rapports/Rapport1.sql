52ed0222117ebb90a453eda76f936d7abdc858f4
Antoine THYS <contact@antoinethys.com>
2019-05-22 16:59:47 +0200

    commit

--- Rapports/Rapport1.sql
+++ Rapports/Rapport1.sql
@@ -1,10 +1,20 @@
 -- OLVoyage souhaite connaître son meilleur employé. Affichez le prénom et le nom de l’employéqui a enregistréle plus grand nombre de réservations.
 -- Si plusieurs employés ont le même nombre de réservations, vous les afficherez tous.
-SELECT *
-FROM ( SELECT UPPER(last_name) ||' '||first_name "Nom", count(reservation_id) "Nombre de reservations"
- FROM T_RESERVATION r
- JOIN T_EMPLOYEE e
- ON r.employee_id = e.employee_id
- GROUP BY last_name, first_name
- ORDER BY count(reservation_id) DESC )
- WHERE ROWNUM <= 1;
+SELECT
+  *
+FROM
+  (
+    SELECT
+      UPPER(last_name) || ' ' || first_name "Nom",
+      count(reservation_id) "Nombre de reservations"
+    FROM
+      T_RESERVATION r
+      JOIN T_EMPLOYEE e ON r.employee_id = e.employee_id
+    GROUP BY
+      last_name,
+      first_name
+    ORDER BY
+      count(reservation_id) DESC
+  )
+WHERE
+  ROWNUM <= 1;
--- Rapports/Rapport14.sql
+++ Rapports/Rapport14.sql
@@ -1,3 +1,2 @@
--- Affichez le numéro du train, le nom du train et le nombre de places libres. Ne prendre en compte que les trains qui ont parcouru une distance supérieure à 300 Km le 22/05/2019 et pour lesquels des billets ont été réservés.
-
+-- Affichez le numéro du train, le nom du train et le nombre de places libres. Ne prendre en compte que les trains qui ont parcouru une distance supérieure à300 Km le 22/05/2019 et pour lesquels des billets ont étéréservés.
 -- Triez les résultats par le numéro du train.
