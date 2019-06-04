a32ecba5bec91b0310b60d32f5db9c7402dac99d
Antoine THYS <contact@antoinethys.com>
2019-06-04 17:01:44 +0200

    15

--- Rapports/Rapport15.sql
+++ Rapports/Rapport15.sql
@@ -1,8 +1,13 @@
 -- Suite aux nombreux recrutements, OLVoyage souhaite établir un nouvel organigramme.
 -- Le nombre des réservations enregistrées par les employés doit suivre leur nom et leur prénom indentés.
 -- Etant donnéque le dirigeant ne s’occupe pas des réservations, il ne doit pas apparaitre sur l’organigramme.
+COLUMN NAME FORMAT A20
 SELECT
-  e.last_name || ' ' || e.first_name "Nom    Prénom",
+  LPAD(
+    e.last_name || ' ' || e.first_name,
+    LENGTH(e.last_name || ' ' || e.first_name) +(LEVEL * 2) -2,
+    '   '
+  ) AS NAME,
   NVL(r.cr, 0) "Nb réservations"
 FROM
   t_employee e FULL
