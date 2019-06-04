9bcb0ef8eafe25d48edc012271287a0ff4f7d9a8
Antoine THYS <contact@antoinethys.com>
2019-06-04 15:24:46 +0200

    6 13

--- Rapports/Rapport13.sql
+++ Rapports/Rapport13.sql
@@ -2,3 +2,22 @@
 -- Ne récupérez que les informations sur les réservations payées qui ont étéfaites 20 jours avant la date de départ du train.
 -- Ne prenez en compte que les trains qui partent entre le 15/04/2019 et le 25/04/2019.
 -- Triez les résultats par la date de la réservation.
+
+SELECT
+  t.ticket_id,
+  c.last_name,
+  c.first_name,
+  wt.train_id
+FROM
+  t_ticket t
+  JOIN t_reservation r ON t.reservation_id = r.reservation_id
+  JOIN t_customer c ON r.buyer_id = c.customer_id
+  JOIN t_wagon_train wt ON t.wag_tr_id = wt.wag_tr_id
+  JOIN t_wagon w ON wt.wagon_id = w.wagon_id
+  JOIN t_train tr ON wt.train_id = tr.train_id
+WHERE
+  w.class_type = 1
+  AND c.birth_date > sysdate -9131
+  AND r.creation_date > tr.departure_time -20
+  AND tr.departure_time BETWEEN '15/04/2019'
+  AND '25/01/2019';
--- Rapports/Rapport6.sql
+++ Rapports/Rapport6.sql
@@ -2,3 +2,51 @@
 -- Votre rapport ne contiendra qu’une seule ligne avec un total par colonne.
 -- Indice
 -- Dans cette requête, vous pourrez constater l’effet de deux fonctions COUNT() dans un même SELECT. Pensez àutiliser les sous-requêtes dans le FROM, ça vous aidera énormément ici et dans les rapports suivants.
+SELECT
+  (
+    SELECT
+      COUNT(employee_id)
+    FROM
+      t_employee
+  ) "Employe",
+  (
+    SELECT
+      COUNT(customer_id)
+    FROM
+      t_customer
+  ) "Client",
+  (
+    SELECT
+      ROUND(
+        ((SUM(NVL2(pass_id, 1, 0))) / COUNT(customer_id)) * 100,
+        2
+      )
+    FROM
+      t_customer
+  ) "% ABONNES",
+  (
+    SELECT
+      COUNT(train_id)
+    FROM
+      t_train
+  ) "Train",
+  (
+    SELECT
+      COUNT(station_id)
+    FROM
+      t_station
+  ) "Station",
+  (
+    SELECT
+      COUNT(reservation_id)
+    FROM
+      t_reservation
+  ) "Reservation",
+  (
+    SELECT
+      COUNT(ticket_id)
+    FROM
+      t_ticket
+  ) "Ticket"
+FROM
+  dual;
