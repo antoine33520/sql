2e39b1a85b6d21a8fdc8796f31fe58bafbfb3be3
pierreSALMI <43409960+pierreSALMI@users.noreply.github.com>
2019-06-04 16:19:41 +0200

    Update Rapport16.sql

--- Rapports/Rapport16.sql
+++ Rapports/Rapport16.sql
@@ -1,16 +1,11 @@
 -- Pour remercier ses coordinateurs de ventes, la sociétésouhaite augmenter leur salaire de $100. Affichez le numéro de l’employé, son nom et son prénom, ainsi que le salaire augmenté.
 -- Utilisez les sous-requêtes avancées.
 SELECT
-  e.last_name || ' ' || e.first_name " NAME ",
+  e.last_name || ' ' || e.first_name " NOM PRENOM ",
   e.employee_id,
   e.salary + 100 " SALAIRE + 100 "
 FROM
   t_employee e
 WHERE
-  e.employee_id = ANY (
-    SELECT
-      DISTINCT emp.employee_id
-    FROM
-      t_employee emp
-      JOIN t_reservation r ON r.employee_id = emp.employee_id
+    manager_id IN (SELECT employee_id FROM t_employee WHERE manager_id IS NULL
   );
